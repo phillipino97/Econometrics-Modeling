984c06608e253a37385262bed89d47e0d7ada845
Phillip Driscoll <38273962+phillipino97@users.noreply.github.com>
2021-11-24 08:36:51 -0600

    Add files via upload

---
+++ Library/FileParse.hpp
@@ -0,0 +1,19 @@
+#ifndef FILEPARSE_H
+#define FILEPARSE_H
+
+#include <string>
+#include <vector>
+
+class FileParse {
+
+    public:
+
+        FileParse();
+        std :: vector<double> ReadCsv(std :: string filename);
+
+    private:
+
+
+};
+
+#endif
